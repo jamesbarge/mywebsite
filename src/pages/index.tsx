83832c21a63781dbe13a8425f694287560ad9c33
jamesbarge <39887980+jamesbarge@users.noreply.github.com>
2023-04-16 17:38:46 +0100

    new commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,52 @@
-import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-const Home = () => {
+export default function Home() {
   return (
-    <div
-      className="flex flex-col items-end justify-center h-screen px-10 bg-white text-gray-900"
-      style={{ fontFamily: 'Roboto, sans-serif' }}
-    >
-      <h1 className="text-4xl font-bold mb-4">Hi, I'm James Barge</h1>
-      <p className="text-lg mb-6">
-        I'm a product manager in London. I help make great products at{' '}
-        <a href="#" className="text-blue-600 hover:text-blue-800">
-          Keebo
-        </a>
-        . Before that, I worked at{' '}
-        <a href="#" className="text-blue-600 hover:text-blue-800">
-          Herd
-        </a>
-        ,{' '}
-        <a href="#" className="text-blue-600 hover:text-blue-800">
-          Public Health England
-        </a>
-        , and{' '}
-        <a href="#" className="text-blue-600 hover:text-blue-800">
-          LoveHomeSwap
-        </a>
-        .
-      </p>
-      <div className="flex space-x-4 mb-4">
-        <a
-          href="https://twitter.com/jamesbrge"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-600 hover:text-gray-800"
-        >
-          Twitter
-        </a>
-        <a
-          href="https://instagram.com/ivlcense"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-600 hover:text-gray-800"
-        >
-          Instagram
-        </a>
-        <a
-          href="mailto:jdwbarge@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-gray-600 hover:text-gray-800"
-        >
-          Email
-        </a>
-      </div>
+    <div className={styles.container}>
+      <Head>
+        <title>James Barge - Product Manager</title>
+        <meta name="description" content="James Barge - Product Manager based in London" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          James Barge
+        </h1>
+        <p className={styles.description}>
+          Product Manager based in London
+        </p>
+        <div className={styles.grid}>
+          <a href="https://keebo.com" className={styles.card}>
+            <h2>Currently: Keebo</h2>
+            <p>Learn more about Keebo, where I currently work as a product manager.</p>
+          </a>
+
+          <a href="https://herd.com" className={styles.card}>
+            <h2>Previously: Herd</h2>
+            <p>Learn more about my role as a product manager at Herd.</p>
+          </a>
+
+          <a href="https://www.gov.uk/government/organisations/public-health-england" className={styles.card}>
+            <h2>Public Health England</h2>
+            <p>Learn more about my time working as a product manager for Public Health England.</p>
+          </a>
+
+          <a href="https://lovehomeswap.com" className={styles.card}>
+            <h2>LoveHomeSwap</h2>
+            <p>Learn more about my experience as a product manager at LoveHomeSwap.</p>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <div className={styles.socialLinks}>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="mailto:email@example.com" target="_blank" rel="noopener noreferrer">Email</a>
+        </div>
+      </footer>
     </div>
   );
-};
-
-export default Home;
+}
